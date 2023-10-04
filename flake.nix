3b9fbdba0cf4338e0089309b03a044b554c52e0b
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-10-04 13:58:31 +0000

    Use nix flakes for a faster dev shell

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A flake for development";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = import ./shell.nix { inherit pkgs; };
+      }
+    );
+}

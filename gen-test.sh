0c1a4cec11d2f53f821717399d6ee8cf95dd9480
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-05-27 10:06:00 +0200

    Add Trustmark example

--- gen-test.sh
+++ gen-test.sh
@@ -4,6 +4,7 @@ set -e
 
 # Generate various SDKs
 
+npm run dev -- ./example/trustmark.json --module-name Trustmark
 npm run dev ./example/realworld-conduit.yaml
 npm run dev ./example/amadeus-airline-lookup.json
 npm run dev ./example/github-spec.json

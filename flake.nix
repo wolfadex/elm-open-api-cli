20206e71add6ef2e81a9b89c3c01834a4cd4b321
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-07-26 10:14:31 +0200

    Updated flake.lock

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A flake for development";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
 
     flake-utils = {
       url = "github:numtide/flake-utils";

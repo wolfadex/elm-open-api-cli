16b5730c8663de798dc9a00c4ea6421de747c9dc
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-10-02 14:51:21 +0200

    Formatting

--- flake.nix
+++ flake.nix
@@ -3,11 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
-
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-
-    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:

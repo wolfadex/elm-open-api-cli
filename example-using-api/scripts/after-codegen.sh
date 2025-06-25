5b0723056d5695879e9a66194077d12c6118079b
Adam DiCarlo <adam@adaptiv.systems>
2025-06-24 17:12:18 -0700

    Clean up, add back elm-codegen for package folder script

--- example-using-api/scripts/after-codegen.sh
+++ example-using-api/scripts/after-codegen.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 set -e
+echo "Example shell script that runs after OpenAPI codegen."
--- example-using-api/scripts/before-codegen.sh
+++ example-using-api/scripts/before-codegen.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 set -e
+echo "Example shell script that runs before OpenAPI codegen."

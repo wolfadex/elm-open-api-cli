f08ef02eb30b15dcd0aa70af08188874da004ae4
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-09-17 17:22:30 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/5ab036a8d97cb9476fbe81b09076e6e91d15e1b6' (2025-06-30)
      → 'github:NixOS/nixpkgs/126caa58957300de7fdd6c7813f45b46ae1d2f23' (2025-09-17)

--- shell.nix
+++ shell.nix
@@ -1,11 +1,13 @@
-{ pkgs ? import <nixpkgs> { } }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 pkgs.mkShell {
   buildInputs = [
     pkgs.elmPackages.elm
     pkgs.elmPackages.elm-format
     pkgs.elmPackages.elm-test-rs
     pkgs.elmPackages.elm-review
-    pkgs.nodejs-18_x
+    pkgs.nodejs_20
     pkgs.git
     pkgs.bash
   ];
